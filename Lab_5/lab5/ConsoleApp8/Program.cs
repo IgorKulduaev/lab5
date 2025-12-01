cbf944b4e910b13f3a64f75e49c3e1ae23c4cbb6
IgorKulduaev <and.sobol39@mail.ru>
2025-12-01 13:34:43 +0200

    Update Program.cs

--- Lab_5/lab5/ConsoleApp8/Program.cs
+++ Lab_5/lab5/ConsoleApp8/Program.cs
@@ -1,23 +1,76 @@
 //Вариант 7 Средний уровень Символьный тип
+// try
+// {
+//     string inputText= Console.ReadLine();
+//     char[] words = inputText.ToCharArray();
+//     Console.WriteLine();
+//     string text = new string(words);
+//     char[] split = new char[] {' ', '.'};
+//     string[] words2 = text.Split(split, StringSplitOptions.RemoveEmptyEntries);
+//     foreach (string word in words2)
+//     {
+//         if (word.Length == 3)
+//         {
+//             Console.WriteLine(word);
+//         }
+//     }
+// }
+// catch (Exception e)
+// {
+//     Console.WriteLine(e.Message);
+// }
+// Вариант 12 Средний урвоень Строковый тип
 try
 {
-    string inputText= Console.ReadLine();
-    char[] words = inputText.ToCharArray();
-    Console.WriteLine();
-    string text = new string(words);
-    char[] split = new char[] {' ', '.'};
-    string[] words2 = text.Split(split, StringSplitOptions.RemoveEmptyEntries);
-    foreach (string word in words2)
+    Console.Write("Введите количество футболистов:");
+    int n = int.Parse(Console.ReadLine());
+    futbol[] people = new futbol[n];
+    for (int i = 0; i < people.Length; i++)
     {
-        if (word.Length == 3)
+        Console.WriteLine($"Введите данные об {i + 1} футболисте:");
+        Console.Write("Введите фамилию футболиста:");
+        people[i].family = Console.ReadLine();
+        Console.Write("Введите дату рождения сотрудника:");
+        people[i].bornData = DateTime.Parse(Console.ReadLine());
+        Console.Write("Введите амплуа футболиста:");
+        people[i].bornMesto = Console.ReadLine();
+        Console.Write("Введите место рождения футболиста:");
+        people[i].amplua = Console.ReadLine();
+        Console.Write("Введите количество игр футболиста:");
+        people[i].kolIgr = int.Parse(Console.ReadLine());
+        Console.Write("Введите количесво желтых карточек:");
+        people[i].kolYellowCard = int.Parse(Console.ReadLine());
+    }
+    foreach (futbol fut in people)
+    {
+        if (fut.kolIgr / 10 >= fut.kolYellowCard && fut.vozr() > 20)
         {
-            Console.WriteLine(word);
+            fut.Print();
         }
     }
 }
-catch (Exception e)
+catch (Exception ex)
 {
-    Console.WriteLine(e.Message);
+    Console.WriteLine(ex.Message);
 }
+struct futbol
+{
+    public string family;
+    public DateTime bornData;
+    public string bornMesto;
+    public string amplua;
+    public int kolIgr;
+    public int kolYellowCard;
+    public int vozr()
+    {
+        return Convert.ToInt32((DateTime.Today - bornData).Days / 365.25);
+    }
+    public void Print()
+    {
+        Console.WriteLine($"{family} {bornData} {bornMesto} {amplua}" +
+            $"{kolIgr} {kolYellowCard} {vozr}");
+    }
+};
+
 
 
